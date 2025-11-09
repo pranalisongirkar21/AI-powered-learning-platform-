74b6b37b1d861dc37a8e86e8593863853dc9b5af
Pranali Songirkar <146335729+pranalisongirkar21@users.noreply.github.com>
2025-11-10 00:19:01 +0530

    Disable things
    some changes

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
@@ -9,8 +8,7 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 8080,
   },
-  // Disable lovable tagging when running in development (localhost).
-  // The plugin will only be active for non-development modes.
+  
   plugins: [react(), mode !== "development" && componentTagger()].filter(
     Boolean
   ),
