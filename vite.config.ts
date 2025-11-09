9b7cd6e6f209eb98d6d007dac07c86ce5cee74dc
Pranali Songirkar <146335729+pranalisongirkar21@users.noreply.github.com>
2025-11-10 00:07:00 +0530

    Add files via upload

---
+++ vite.config.ts
@@ -0,0 +1,22 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
+import { componentTagger } from "lovable-tagger";
+
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => ({
+  server: {
+    host: "::",
+    port: 8080,
+  },
+  // Disable lovable tagging when running in development (localhost).
+  // The plugin will only be active for non-development modes.
+  plugins: [react(), mode !== "development" && componentTagger()].filter(
+    Boolean
+  ),
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+}));
